7e748777c82fe7490e6311ab5f10daba5851fe92
serge <sergejnazarov3728@gmail.com>
2025-05-11 22:34:13 +0400

    Начата работа над сайтом.

---
+++ BlazorApp5/Enums/ServiceType.cs
@@ -0,0 +1,15 @@
+namespace BlazorApp5.Enums;
+
+public enum ServiceType
+{
+    EngineRepair,
+    BatteryRepair,
+    SuspensionRepair,
+    AutopartsReplacement,
+    CoolingSystemRepair,
+    Diagnostics,
+    TireService,
+    PowerSteeringRepair,
+    CarBodyRepair,
+    Painting
+}
---
+++ BlazorApp5/Models/Service.cs
@@ -0,0 +1,12 @@
+using BlazorApp5.Enums;
+
+namespace BlazorApp5.Models;
+
+public class Service
+{
+    public int Id { get; set; }
+    public ServiceType ServiceType { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    public decimal Price { get; set; }
+}
